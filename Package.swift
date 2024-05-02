e35fc764c44c8ef3225ce4fd9cc99f7b72690722
pollini <marius.pollin@googlemail.com>
2024-05-02 17:52:52 +0200

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version: 5.10
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "SurveyNative-SPM",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "SurveyNative-SPM",
+            targets: ["SurveyNative-SPM"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "SurveyNative-SPM"),
+        .testTarget(
+            name: "SurveyNative-SPMTests",
+            dependencies: ["SurveyNative-SPM"]),
+    ]
+)
